93d4cc3d114456c1ea098911fa880bf464e1df4f
ysera34 <ycyoon34@gmail.com>
2016-12-02 02:42:32 +0900

    faq expandable list

--- Chemi_v04/app/src/main/java/com/tacademy/v04/chemi/view/fragment/navigation/FAQFragment.java
+++ Chemi_v04/app/src/main/java/com/tacademy/v04/chemi/view/fragment/navigation/FAQFragment.java
@@ -6,8 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 
 import com.tacademy.v04.chemi.R;
+import com.tacademy.v04.chemi.common.util.adapter.ExpandableListAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by yoon on 2016. 11. 15..
@@ -15,6 +20,11 @@ import com.tacademy.v04.chemi.R;
  */
 
 public class FAQFragment extends Fragment {
+
+    private ExpandableListAdapter mExpandableListAdapter;
+    private ExpandableListView mExpandableListView;
+    private ArrayList<String> mHeaderDataList;
+    private HashMap<String, ArrayList<String>> mBodyDataMap;
 
     public FAQFragment() {
     }
@@ -28,6 +38,8 @@ public class FAQFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mHeaderDataList = new ArrayList<>();
+        mBodyDataMap = new HashMap<>();
     }
 
     @Nullable
@@ -36,11 +48,50 @@ public class FAQFragment extends Fragment {
                              @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 //        return super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.fragment_faq, container, false);
+        mExpandableListView = (ExpandableListView) view.findViewById(R.id.faq_expandable_list_view);
+
+        setupFAQData();
+
+        mExpandableListAdapter = new ExpandableListAdapter(getActivity(), mHeaderDataList, mBodyDataMap);
+        mExpandableListView.setAdapter(mExpandableListAdapter);
+
         return view;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    private void setupFAQData() {
+        String[] headerTextArr = getResources().getStringArray(R.array.faq_header_list);
+        String[] bodyTextArr = getResources().getStringArray(R.array.faq_body_list);
+
+        for (int i = 0; i < headerTextArr.length; i++) {
+            mHeaderDataList.add(headerTextArr[i]);
+        }
+
+        ArrayList<String> body1 = new ArrayList<>();
+        body1.add(bodyTextArr[0]);
+        ArrayList<String> body2 = new ArrayList<>();
+        body2.add(bodyTextArr[1]);
+        ArrayList<String> body3 = new ArrayList<>();
+        body3.add(bodyTextArr[2]);
+        ArrayList<String> body4 = new ArrayList<>();
+        body4.add(bodyTextArr[3]);
+        ArrayList<String> body5 = new ArrayList<>();
+        body5.add(bodyTextArr[4]);
+        ArrayList<String> body6 = new ArrayList<>();
+        body6.add(bodyTextArr[5]);
+        ArrayList<String> body7 = new ArrayList<>();
+        body7.add(bodyTextArr[6]);
+
+        mBodyDataMap.put(mHeaderDataList.get(0), body1);
+        mBodyDataMap.put(mHeaderDataList.get(1), body2);
+        mBodyDataMap.put(mHeaderDataList.get(2), body3);
+        mBodyDataMap.put(mHeaderDataList.get(3), body4);
+        mBodyDataMap.put(mHeaderDataList.get(4), body5);
+        mBodyDataMap.put(mHeaderDataList.get(5), body6);
+        mBodyDataMap.put(mHeaderDataList.get(6), body7);
     }
 }
