005000dc0f064616aadc2bfba50b50f6c1e735ba
ysera34 <ycyoon34@gmail.com>
2016-12-02 21:25:26 +0900

    archive viewpager

--- Chemi_v04/app/src/main/java/com/tacademy/v04/chemi/view/fragment/navigation/ArchiveContentTabFragment.java
+++ Chemi_v04/app/src/main/java/com/tacademy/v04/chemi/view/fragment/navigation/ArchiveContentTabFragment.java
@@ -2,12 +2,18 @@ package com.tacademy.v04.chemi.view.fragment.navigation;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.tacademy.v04.chemi.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by yoon on 2016. 11. 14..
@@ -15,6 +21,11 @@ import com.tacademy.v04.chemi.R;
  */
 
 public class ArchiveContentTabFragment extends Fragment {
+
+    private TabLayout mArchiveContentTabLayout;
+    private ViewPager mArchiveContentViewPager;
+    private ArrayList<Fragment> mArchiveContentListFragments;
+    private ArrayList<String> mArchiveContentFragmentsTitles;
 
     public ArchiveContentTabFragment() {
     }
@@ -28,7 +39,15 @@ public class ArchiveContentTabFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mArchiveContentListFragments = new ArrayList<>();
+        mArchiveContentFragmentsTitles = new ArrayList<>();
+
+        addArchiveContentListFragment(ArchiveContentT1ListFragment.newInstance(1), "케미데스크");
+        addArchiveContentListFragment(ArchiveContentT2ListFragment.newInstance(2), "케미 PICK");
+        addArchiveContentListFragment(ArchiveContentT3ListFragment.newInstance(3), "생활 정보");
     }
+
 
     @Nullable
     @Override
@@ -36,6 +55,33 @@ public class ArchiveContentTabFragment extends Fragment {
                              @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 //        return super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.fragment_archive_content_tab, container, false);
+
+        mArchiveContentTabLayout = (TabLayout) view.findViewById(R.id.archive_content_tabLayout);
+        mArchiveContentViewPager = (ViewPager) view.findViewById(R.id.archive_content_view_pager);
+
+        FragmentManager fm = getChildFragmentManager();
+
+        mArchiveContentViewPager.setOffscreenPageLimit(2);
+        mArchiveContentViewPager.setAdapter(new FragmentPagerAdapter(fm) {
+            @Override
+            public Fragment getItem(int position) {
+                return mArchiveContentListFragments.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return mArchiveContentListFragments.size();
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+//                return super.getPageTitle(position);
+                return mArchiveContentFragmentsTitles.get(position);
+            }
+        });
+
+        mArchiveContentTabLayout.setupWithViewPager(mArchiveContentViewPager);
+
         return view;
     }
 
@@ -43,4 +89,10 @@ public class ArchiveContentTabFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
     }
+
+    private void addArchiveContentListFragment(Fragment fragment, String title) {
+        mArchiveContentListFragments.add(fragment);
+        mArchiveContentFragmentsTitles.add(title);
+    }
+
 }
